dd3c74817a349e7c7a481b7c76c8cb7abdef94c1
markusmichel <markus.michel.1988@googlemail.com>
2015-03-03 12:31:36 +0100

    Install phpmyadmin on provision

--- Vagrant_provision.sh
+++ Vagrant_provision.sh
@@ -61,6 +61,38 @@ phpunit --version
 sudo service apache2 reload
 
 
+# If phpmyadmin does not exist
+if [ ! -f /etc/phpmyadmin/config.inc.php ];
+then
+
+	# Used debconf-get-selections to find out what questions will be asked
+	# This command needs debconf-utils
+
+	# Handy for debugging. clear answers phpmyadmin: echo PURGE | debconf-communicate phpmyadmin
+
+	echo 'phpmyadmin phpmyadmin/dbconfig-install boolean false' | debconf-set-selections
+	echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+
+	echo 'phpmyadmin phpmyadmin/app-password-confirm password root' | debconf-set-selections
+	echo 'phpmyadmin phpmyadmin/mysql/admin-pass password root' | debconf-set-selections
+	echo 'phpmyadmin phpmyadmin/password-confirm password root' | debconf-set-selections
+	echo 'phpmyadmin phpmyadmin/setup-password password root' | debconf-set-selections
+	echo 'phpmyadmin phpmyadmin/database-type select mysql' | debconf-set-selections
+	echo 'phpmyadmin phpmyadmin/mysql/app-pass password root' | debconf-set-selections
+
+	echo 'dbconfig-common dbconfig-common/mysql/app-pass password root' | debconf-set-selections
+	echo 'dbconfig-common dbconfig-common/mysql/app-pass password' | debconf-set-selections
+	echo 'dbconfig-common dbconfig-common/password-confirm password root' | debconf-set-selections
+	echo 'dbconfig-common dbconfig-common/app-password-confirm password root' | debconf-set-selections
+	echo 'dbconfig-common dbconfig-common/app-password-confirm password root' | debconf-set-selections
+	echo 'dbconfig-common dbconfig-common/password-confirm password root' | debconf-set-selections
+
+	apt-get -y install phpmyadmin
+
+	sudo ln -s /usr/share/phpmyadmin/ /var/www/phpmyadmin
+fi
+
+
 cd /var/www
 php -r "readfile('https://getcomposer.org/installer');" | php
 php composer.phar install

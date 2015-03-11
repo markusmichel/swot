720d10f6084419a8a455f714a2835e9e92a866f4
Markus Michel <markus.michel.1988@gmail.com>
2015-03-11 12:19:29 +0100

    Created basic break up form = delete friendship

--- src/Swot/NetworkBundle/Controller/FriendController.php
+++ src/Swot/NetworkBundle/Controller/FriendController.php
@@ -3,8 +3,11 @@
 namespace Swot\NetworkBundle\Controller;
 
 use Swot\NetworkBundle\Entity\User;
+use Swot\NetworkBundle\Security\UserVoter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class FriendController extends Controller
 {
@@ -24,12 +27,74 @@ class FriendController extends Controller
 
     public function showAction($id)
     {
+        /** @var Translator $translator */
+        $translator = $this->get('translator');
+
         /** @var User $user */
         $user = $this->getDoctrine()->getRepository('SwotNetworkBundle:User')->find($id);
 
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
+        if($user === null) {
+            $this->addFlash('notice', $translator->trans('user.profile.show.does_not_exist'));
+            return $this->redirectToRoute('my_friends');
+        }
+
+        if(!$this->isGranted(UserVoter::SHOW, $user)) {
+            $this->addFlash('notice', $translator->trans('user.profile.show.not_authed'));
+            return $this->redirectToRoute('my_friends');
+        }
+
         return $this->render('SwotNetworkBundle:Friend:show.html.twig', array(
             'user'  => $user,
+            'breakUpForm' => $this->createRemoveFriendshipForm($user)->createView(),
         ));
+    }
+
+    public function removeFriendshipAction(Request $request, $id) {
+        /** @var Translator $translator */
+        $translator = $this->get('translator');
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        /** @var User $friend */
+        $friend = $this->getDoctrine()->getRepository('SwotNetworkBundle:User')->find($id);
+
+        if($friend === null) {
+            $this->addFlash('notice', $translator->trans('User does not exist'));
+            return $this->redirectToRoute('my_friends');
+        }
+
+        $form = $this->createRemoveFriendshipForm($friend);
+        $form->handleRequest($request);
+
+        if(!$form->isValid() || !$user->isFriendOf($friend)) {
+            return new Response("invalid");
+        }
+
+        return new Response("valid");
+    }
+
+    /**
+     * @param $user User
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createRemoveFriendshipForm($user) {
+        $translator = $this->get('translator');
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('friend_remove_friendship', array('id' => $user->getId())))
+            ->setMethod('POST')
+            ->add('submit', 'submit', array(
+                'label' => $translator->trans('Break with %username%', array(
+                        "%username%" => $user->getUsername()
+                    )
+                )
+            ))
+            ->getForm();
+
+        return $form;
     }
 
 }

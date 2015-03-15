e38301f376258e01540294f0f7c349c36c0f6bbc
Markus Michel <markus.michel.1988@gmail.com>
2015-03-15 11:57:13 +0100

    Compress profile image after upload

--- src/Swot/NetworkBundle/File/ImageUploader.php
+++ src/Swot/NetworkBundle/File/ImageUploader.php
@@ -3,6 +3,8 @@
 namespace Swot\NetworkBundle\File;
 
 
+use Liip\ImagineBundle\Imagine\Data\DataManager;
+use Liip\ImagineBundle\Imagine\Filter\FilterManager;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ImageUploader implements FileUploadInterface {
@@ -13,10 +15,36 @@ class ImageUploader implements FileUploadInterface {
 
     private $file;
 
+    private $dataManager;
+    private $filterManager;
 
-    public function __construct($kernelDir, $uploadDir) {
+    public function __construct($kernelDir, $uploadDir, DataManager $dataManager, FilterManager $filterManager) {
         $this->webDir = $kernelDir . '/../web/';
         $this->uploadDir = $uploadDir;
+
+        $this->dataManager = $dataManager;
+        $this->filterManager = $filterManager;
+    }
+
+    /**
+     * Resizes an image.
+     * @param $imagePath Path + filename of the image file to resize
+     * @param $filterName Name of the liip imagine filter to apply
+     */
+    private function resizeImage($imagePath, $filterName) {
+        $dataManager   = $this->dataManager;
+        $filterManager = $this->filterManager;
+
+        // find the image and determine its type
+        $image = $dataManager->find($filterName, $imagePath);
+
+        $response = $filterManager->applyFilter($image, $filterName);
+        $thumb = $response->getContent();
+
+        // create and write new image file
+        $f = fopen($imagePath, 'w');
+        fwrite($f, $thumb);
+        fclose($f);
     }
 
     private function generateFilename() {
@@ -38,11 +66,7 @@ class ImageUploader implements FileUploadInterface {
             $filename
         );
 
-        // set the path property to the filename where you've saved the file
-        $this->profileImage = $this->getFile()->getClientOriginalName();
-
-        // clean up the file property as you won't need it anymore
-        $this->profileImageFile = null;
+        $this->resizeImage($this->getUploadDirRelative() . $filename, 'profile_image');
 
         return $filename;
     }

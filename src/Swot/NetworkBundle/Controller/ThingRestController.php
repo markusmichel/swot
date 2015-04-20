dd45d5c6d617b13aa78c5a2de1a45496b594dd70
ElwoodSlut <markus.uderhardt@stud.uni-regensburg.de>
2015-04-20 21:27:34 +0200

    updated thingrestcontroller apidoc

--- src/Swot/NetworkBundle/Controller/ThingRestController.php
+++ src/Swot/NetworkBundle/Controller/ThingRestController.php
@@ -40,6 +40,15 @@ class ThingRestController extends FOSRestController
      *
      * @param ParamFetcher $fetcher
      * @return Response
+     *
+     * @api {post} /api/v1/thing/messages Message receive route.
+     * @apiGroup ThingRestController
+     * @apiName PostMessage
+     * @apiParam {ParamFetcher} fetcher Array which contains the given params. In this case a "message" parameter has to be sent.
+     * @apiParam {RequestParameter} message A field in the sent request. It contains the message to be saved.
+     * @apiSuccess {Response} response The response to be sent. Contains the 200 response code and a short message.
+     * @apiDescription Receives a message from a thing. Saves it to the thing's message list.
+     * @apiVersion 0.1.0
      */
     public function postMessageAction(ParamFetcher $fetcher) {
         /** @var Thing $thing */
@@ -68,6 +77,15 @@ class ThingRestController extends FOSRestController
      *
      * @param ParamFetcher $fetcher
      * @return Response
+     *
+     * @api {post} /api/v1/thing/functions/update Update thing-functions notification.
+     * @apiGroup ThingRestController
+     * @apiName PostThingFunctionsUpdate
+     * @apiParam {ParamFetcher} fetcher Array which contains the given params.
+     * @apiSuccess {Response} response The response to be sent. Contains the 200 response code and a short message.
+     * @apiDescription Receives an update notification from the thing and updates the saved functions with the current ones.
+     * To get the updated data, a cURL call to the thing is made.
+     * @apiVersion 0.1.0
      */
     public function postThingFunctionsUpdateAction(ParamFetcher $fetcher) {
         /** @var Thing $thing */
@@ -116,6 +134,16 @@ class ThingRestController extends FOSRestController
      *
      * @param ParamFetcher $fetcher
      * @return Response
+     *
+     * @api {post} /api/v1/thing/information/update Update thing-information notification.
+     * @apiGroup ThingRestController
+     * @apiName PostThingInformationUpdate
+     * @apiParam {ParamFetcher} fetcher Array which contains the given params.
+     * @apiParam {RequestParameter} information A field in the sent request.
+     * @apiSuccess {Response} response The response to be sent. Contains the 200 response code and a short message.
+     * @apiDescription Receives an update notification from the thing and updates the saved information with the current ones.
+     * To get the updated data, a cURL call to the thing is made.
+     * @apiVersion 0.1.0
      */
     public function postThingInformationUpdateAction(ParamFetcher $fetcher) {
         /** @var Thing $thing */
@@ -153,6 +181,16 @@ class ThingRestController extends FOSRestController
      *
      * @param ParamFetcher $fetcher
      * @return Response
+     *
+     * @api {post} /api/v1/thing/profileimage/update Update thing profile image notification.
+     * @apiGroup ThingRestController
+     * @apiName PostThingProfileImageUpdate
+     * @apiParam {ParamFetcher} fetcher Array which contains the given params. In this case a "profileimage" parameter has to be sent.
+     * @apiParam {RequestParameter} profileimage A field in the sent request. It contains the url for the new profile image.
+     * @apiSuccess {Response} response The response to be sent. The response to be sent. Contains the 200 response code and a short message.
+     * @apiDescription Receives an update notification from the thing and updates the saved profile image with the current one.
+     * To get the updated image, a cURL call to the thing is made.
+     * @apiVersion 0.1.0
      */
     public function postThingProfileImageUpdateAction(ParamFetcher $fetcher) {
         /** @var Thing $thing */
